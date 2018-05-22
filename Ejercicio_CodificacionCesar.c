7d350196174ccf2dfa51635f58a72ab59370400f
ignacio-git <39530036+ignacio-git@users.noreply.github.com>
2018-05-22 12:28:44 -0500

    Add files via upload

---
+++ Ejercicio_CodificacionCesar.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  Ejercicio_CodificacionCesar
+//
+//  Created by Itamar Vazquez on 22.05.18.
+//  Copyright © 2018 Ignacio Itamar Vazquez Martinez. All rights reserved.
+//  97-122
+
+#include <stdio.h>
+#define enter printf("\n");
+int main() {
+    char array[100];
+    scanf("%[^\n]%*c",array);
+    printf("%s",array);
+    enter
+    for(int i=0; i<strlen(array); i++){
+        if(array[i] != 32){
+            if(array[i] == 122){
+                printf("%c",array[i]-25);
+            }
+            printf("%c",array[i]);
+        }
+    }
+    enter
+    return 0;
+}
